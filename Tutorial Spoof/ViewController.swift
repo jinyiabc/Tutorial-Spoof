9a01559a02178b978b55813be369ecf1748480d8
Alex J.Y <33644601+jinyiabc@users.noreply.github.com>
2020-10-08 02:19:41 +0800

    Initial Commit

---
+++ Tutorial Spoof/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tutorial Spoof
+//
+//  Created by jinyi on 2020/10/8.
+//  Copyright © 2020 jinyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Tutorial SpoofTests/Tutorial_SpoofTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tutorial_SpoofTests.swift
+//  Tutorial SpoofTests
+//
+//  Created by jinyi on 2020/10/8.
+//  Copyright © 2020 jinyi. All rights reserved.
+//
+
+import XCTest
+@testable import Tutorial_Spoof
+
+class Tutorial_SpoofTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
